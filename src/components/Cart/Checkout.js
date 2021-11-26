2c0a9c4720f9fc53c014032d1ea21140870d41d7
camille-granier <granierc@protonmail.com>
2021-11-26 14:41:02 +0100

    modifying price style

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,30 +1,36 @@
 import classes from './Checkout.module.css';
-import React from 'react';
 
 const Checkout = (props) => {
+  const confirmHandler = (e) => {
+    e.preventDefault();
+  };
 
-  const confirmHandler = (e) => {};
-
-  return (<form onSubmit={confirmHandler} className={classes.control}>
-    <div>
-      <label htmlFor="Name">Your Name</label>
-      <input type="text" id="name" />
-    </div>
-    <div>
-      <label htmlFor="street">Street</label>
-      <input type="text" id="street" />
-    </div>
-    <div>
-      <label htmlFor="postal">Postal Code</label>
-      <input type="text" id="postal" />
-    </div>
-    <div>
-      <label htmlFor="city">City</label>
-      <input type="text" id="city" />
-    </div>
-    <button type="button" onClick={props.onCancel}>Cancel</button>
-    <button>Confirm</button>
-  </form>)
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' />
+      </div>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
 };
 
 export default Checkout;
